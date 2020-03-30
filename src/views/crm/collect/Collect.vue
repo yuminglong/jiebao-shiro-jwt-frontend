044818b78a50b6dc52834eff423ba27e0ae19968
yml <1367422552@qq.com>
2020-03-30 12:51:02 +0800

    eslint

--- src/views/crm/collect/Collect.vue
+++ src/views/crm/collect/Collect.vue
@@ -58,79 +58,79 @@
 </template>
 
 <script>
-  import RangeDate from '../../../components/datetime/RangeDate'
-  export default {
-    name: 'Collect',
-    components: {RangeDate},
-    data () {
-      return {
-        advanced: false,
-        dataSource: [],
-        selectedRowKeys: [],
-        queryParams: {},
-        filteredInfo: null,
-        sortedInfo: null,
-        paginationInfo: null,
-        pagination: {
-          pageSizeOptions: ['10', '20', '30', '40', '100'],
-          defaultCurrent: 1,
-          defaultPageSize: 10,
-          showQuickJumper: true,
-          showSizeChanger: true,
-          showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
-        },
-        loading: false
+import RangeDate from '../../../components/datetime/RangeDate'
+export default {
+  name: 'Collect',
+  components: {RangeDate},
+  data () {
+    return {
+      advanced: false,
+      dataSource: [],
+      selectedRowKeys: [],
+      queryParams: {},
+      filteredInfo: null,
+      sortedInfo: null,
+      paginationInfo: null,
+      pagination: {
+        pageSizeOptions: ['10', '20', '30', '40', '100'],
+        defaultCurrent: 1,
+        defaultPageSize: 10,
+        showQuickJumper: true,
+        showSizeChanger: true,
+        showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+      },
+      loading: false
+    }
+  },
+  computed: {
+    columns () {
+      return [{
+        title: '合同单号',
+        dataIndex: 'ht_id'
+      }, {
+        title: '客户名称',
+        dataIndex: 'ht_customerid'
+      }, {
+        title: '项目名称',
+        dataIndex: 'ht_title'
+      }, {
+        title: '所有者',
+        dataIndex: 'ht_preside'
+      }, {
+        title: '合同签订日期',
+        dataIndex: 'ht_date'
+      }, {
+        title: '合同金额',
+        dataIndex: 'ht_summoney'
+      }, {
+        title: '已收款金额',
+        dataIndex: 'afterMoney'
+      }, {
+        title: '待收款金额',
+        dataIndex: 'waitMoney'
+      }]
+    }
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+      if (!this.advanced) {
+        this.queryParams.createTimeFrom = ''
+        this.queryParams.createTimeTo = ''
       }
     },
-    computed: {
-      columns () {
-        return [{
-          title: '合同单号',
-          dataIndex: 'ht_id'
-        }, {
-          title: '客户名称',
-          dataIndex: 'ht_customerid'
-        }, {
-          title: '项目名称',
-          dataIndex: 'ht_title'
-        }, {
-            title: '所有者',
-            dataIndex: 'ht_preside'
-        }, {
-          title: '合同签订日期',
-          dataIndex: 'ht_date'
-        }, {
-          title: '合同金额',
-          dataIndex: 'ht_summoney'
-        }, {
-          title: '已收款金额',
-          dataIndex: 'afterMoney'
-        }, {
-          title: '待收款金额',
-          dataIndex: 'waitMoney'
-        }]
+    onSelectChange (selectedRowKeys) {
+      this.selectedRowKeys = selectedRowKeys
+    },
+    handleDateChange (value) {
+      if (value) {
+        this.queryParams.createTimeFrom = value[0]
+        this.queryParams.createTimeTo = value[1]
       }
     },
-    mounted () {
-      this.fetch()
-    },
-    methods: {
-      toggleAdvanced () {
-        this.advanced = !this.advanced
-        if (!this.advanced) {
-          this.queryParams.createTimeFrom = ''
-          this.queryParams.createTimeTo = ''
-        }
-      },
-      onSelectChange (selectedRowKeys) {
-        this.selectedRowKeys = selectedRowKeys
-      },
-      handleDateChange (value) {
-        if (value) {
-          this.queryParams.createTimeFrom = value[0]
-          this.queryParams.createTimeTo = value[1]
-        }
-      },
     /*  exportExcel () {
         let {sortedInfo} = this
         let sortField, sortOrder
@@ -144,77 +144,77 @@
           sortOrder: sortOrder,
           ...this.queryParams
         })
-      }, */
-      search () {
-        let {sortedInfo} = this
-        let sortField, sortOrder
-        // 获取当前列的排序和列的过滤规则
-        if (sortedInfo) {
-          sortField = sortedInfo.field
-          sortOrder = sortedInfo.order
-        }
-        this.fetch({
-          sortField: sortField,
-          sortOrder: sortOrder,
-          ...this.queryParams
-        })
-      },
-      reset () {
-        // 取消选中
-        this.selectedRowKeys = []
-        // 重置分页
-        this.pagination.current = this.pagination.defaultCurrent
-        if (this.paginationInfo) {
-          this.paginationInfo.current = this.pagination.defaultCurrent
-          this.paginationInfo.pageSize = this.pagination.defaultPageSize
-        }
-        // 重置列排序规则
-        this.sortedInfo = null
-        // 重置查询参数
-        this.queryParams = {}
-        // 清空时间选择
-        if (this.advanced) {
-          this.$refs.ht.reset()
-        }
-        this.fetch()
-      },
-      handleTableChange (pagination, filters, sorter) {
-        this.paginationInfo = pagination
-        this.filteredInfo = filters
-        this.sortedInfo = sorter
-        this.fetch({
-          sortField: sorter.field,
-          sortOrder: sorter.order,
-          ...this.queryParams,
-          ...filters
-        })
-      },
-      fetch (params = {}) {
-        this.loading = true
-        if (this.paginationInfo) {
-          // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
-          this.pagination.current = this.paginationInfo.current
-          this.pagination.pageSize = this.paginationInfo.pageSize
-          params.pageSize = this.paginationInfo.pageSize
-          params.pageNum = this.paginationInfo.current
-        } else {
-          // 如果分页信息为空，则设置为默认值
-          params.pageSize = this.pagination.defaultPageSize
-          params.pageNum = this.pagination.defaultCurrent
-        }
-        this.$get('/crm/contract-order', {
-          ...params
-        }).then((r) => {
-          let data = r.data
-          const pagination = {...this.pagination}
-          this.dataSource = data
-          this.pagination = pagination
-          // 数据加载完毕，关闭loading
-          this.loading = false
-        })
+      },  */
+    search () {
+      let {sortedInfo} = this
+      let sortField, sortOrder
+      // 获取当前列的排序和列的过滤规则
+      if (sortedInfo) {
+        sortField = sortedInfo.field
+        sortOrder = sortedInfo.order
       }
+      this.fetch({
+        sortField: sortField,
+        sortOrder: sortOrder,
+        ...this.queryParams
+      })
+    },
+    reset () {
+      // 取消选中
+      this.selectedRowKeys = []
+      // 重置分页
+      this.pagination.current = this.pagination.defaultCurrent
+      if (this.paginationInfo) {
+        this.paginationInfo.current = this.pagination.defaultCurrent
+        this.paginationInfo.pageSize = this.pagination.defaultPageSize
+      }
+      // 重置列排序规则
+      this.sortedInfo = null
+      // 重置查询参数
+      this.queryParams = {}
+      // 清空时间选择
+      if (this.advanced) {
+        this.$refs.ht.reset()
+      }
+      this.fetch()
+    },
+    handleTableChange (pagination, filters, sorter) {
+      this.paginationInfo = pagination
+      this.filteredInfo = filters
+      this.sortedInfo = sorter
+      this.fetch({
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...this.queryParams,
+        ...filters
+      })
+    },
+    fetch (params = {}) {
+      this.loading = true
+      if (this.paginationInfo) {
+        // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
+        this.pagination.current = this.paginationInfo.current
+        this.pagination.pageSize = this.paginationInfo.pageSize
+        params.pageSize = this.paginationInfo.pageSize
+        params.pageNum = this.paginationInfo.current
+      } else {
+        // 如果分页信息为空，则设置为默认值
+        params.pageSize = this.pagination.defaultPageSize
+        params.pageNum = this.pagination.defaultCurrent
+      }
+      this.$get('/crm/contract-order', {
+        ...params
+      }).then((r) => {
+        let data = r.data
+        const pagination = {...this.pagination}
+        this.dataSource = data
+        this.pagination = pagination
+        // 数据加载完毕，关闭loading
+        this.loading = false
+      })
     }
   }
+}
 </script>
 
 <style lang="less" scoped>
--- src/views/crm/ticket/Ticket.vue
+++ src/views/crm/ticket/Ticket.vue
@@ -58,7 +58,7 @@
                :loading="loading"
                :scroll="{ x: 900 }"
                @change="handleTableChange">
-        <template slot="operation" slot-scope="text, record">
+        <template slot="operation" slot-scope="text,record">
           <a-icon  type="eye" theme="twoTone" twoToneColor="#42b983"
                    @click="view(record)" title="订单详情"></a-icon>
         </template>
@@ -75,104 +75,104 @@
 </template>
 
 <script>
-  import RangeDate from '../../../components/datetime/RangeDate'
-  import TicketInfo from './TicketInfo'
-  export default {
-    name: 'Ticket',
-    components: {TicketInfo, RangeDate},
-    data () {
-      return {
-        ticketInfo: {
-          visiable: false,
-          data: {}
-        },
-        advanced: false,
-        dataSource: [],
-        selectedRowKeys: [],
-        queryParams: {},
-        filteredInfo: null,
-        sortedInfo: null,
-        paginationInfo: null,
-        pagination: {
-          pageSizeOptions: ['10', '20', '30', '40', '100'],
-          defaultCurrent: 1,
-          defaultPageSize: 10,
-          showQuickJumper: true,
-          showSizeChanger: true,
-          showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
-        },
-        loading: false
-      }
-    },
-    computed: {
-      columns () {
-        return [{
-          title: '合同单号',
-          dataIndex: 'billParentid'
-        }, {
-          title: '开票金额',
-          dataIndex: 'billMoney'
-        }, {
-          title: '开票号码',
-          dataIndex: 'billNo'
-        }, {
-          title: '开票日期',
-          dataIndex: 'billDate'
-        }, {
-          title: '回款/回款日期',
-          dataIndex: 'billHkid'
-        }, {
-          title: '是否回款',
-          dataIndex: 'billHkstate',
-          customRender: (text) => {
-            switch (text) {
-              case '未回款':
-                return <a-tag color="red">未回款</a-tag>
-              case '已回款':
-                return <a-tag color="cyan">已回款</a-tag>
-              default:
-                return text
-            }
+import RangeDate from '../../../components/datetime/RangeDate'
+import TicketInfo from './TicketInfo'
+export default {
+  name: 'Ticket',
+  components: {TicketInfo, RangeDate},
+  data () {
+    return {
+      ticketInfo: {
+        visiable: false,
+        data: {}
+      },
+      advanced: false,
+      dataSource: [],
+      selectedRowKeys: [],
+      queryParams: {},
+      filteredInfo: null,
+      sortedInfo: null,
+      paginationInfo: null,
+      pagination: {
+        pageSizeOptions: ['10', '20', '30', '40', '100'],
+        defaultCurrent: 1,
+        defaultPageSize: 10,
+        showQuickJumper: true,
+        showSizeChanger: true,
+        showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+      },
+      loading: false
+    }
+  },
+  computed: {
+    columns () {
+      return [{
+        title: '合同单号',
+        dataIndex: 'billParentid'
+      }, {
+        title: '开票金额',
+        dataIndex: 'billMoney'
+      }, {
+        title: '开票号码',
+        dataIndex: 'billNo'
+      }, {
+        title: '开票日期',
+        dataIndex: 'billDate'
+      }, {
+        title: '回款/回款日期',
+        dataIndex: 'billHkid'
+      }, {
+        title: '是否回款',
+        dataIndex: 'billHkstate',
+        customRender: (text) => {
+          switch (text) {
+            case '未回款':
+              return <a-tag color="red">未回款</a-tag>
+            case '已回款':
+              return <a-tag color="cyan">已回款</a-tag>
+            default:
+              return text
           }
-        }, {
-          title: '备注',
-          dataIndex: 'billRemark'
-        }, {
-          title: '合同详情',
-          dataIndex: 'operation',
-          scopedSlots: { customRender: 'operation' }
         }
-        ]
+      }, {
+        title: '备注',
+        dataIndex: 'billRemark'
+      }, {
+        title: '合同详情',
+        dataIndex: 'operation',
+        scopedSlots: { customRender: 'operation' }
+      }
+      ]
+    }
+  },
+  mounted () {
+    this.fetch()
+  },
+  methods: {
+    handleTicketInfoClose () {
+      this.ticketInfo.visiable = false
+    },
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+      if (!this.advanced) {
+        this.queryParams.createTimeFrom = ''
+        this.queryParams.createTimeTo = ''
       }
     },
-    mounted () {
-      this.fetch()
+    onSelectChange (selectedRowKeys) {
+      this.selectedRowKeys = selectedRowKeys
     },
-    methods: {
-      handleTicketInfoClose () {
-        this.ticketInfo.visiable = false
-      },
-      toggleAdvanced () {
-        this.advanced = !this.advanced
-        if (!this.advanced) {
-          this.queryParams.createTimeFrom = ''
-          this.queryParams.createTimeTo = ''
-        }
-      },
-      onSelectChange (selectedRowKeys) {
-        this.selectedRowKeys = selectedRowKeys
-      },
-      view (record) {
-        this.ticketInfo.data = record
-        this.ticketInfo.visiable = true
-      },
-      handleDateChange (value) {
-        if (value) {
-          this.queryParams.createTimeFrom = value[0]
-          this.queryParams.createTimeTo = value[1]
-        }
-      },
-     /* exportExcel () {
+    view (record) {
+      this.ticketInfo.data = record
+      this.ticketInfo.visiable = true
+    },
+    handleDateChange (value) {
+      if (value) {
+        this.queryParams.createTimeFrom = value[0]
+        this.queryParams.createTimeTo = value[1]
+      }
+    },
+    /* exportExcel () {
         let {sortedInfo} = this
         let sortField, sortOrder
         // 获取当前列的排序和列的过滤规则
@@ -186,82 +186,82 @@
           ...this.queryParams
         })
       }, */
-      search () {
-        this.pagination.current = this.pagination.defaultCurrent
-        if (this.paginationInfo) {
-          this.paginationInfo.current = this.pagination.defaultCurrent
-          this.paginationInfo.pageSize = this.pagination.defaultPageSize
-        }
-        let {sortedInfo} = this
-        let sortField, sortOrder
-        // 获取当前列的排序和列的过滤规则
-        if (sortedInfo) {
-          sortField = sortedInfo.field
-          sortOrder = sortedInfo.order
-        }
-        this.fetch({
-          sortField: sortField,
-          sortOrder: sortOrder,
-          ...this.queryParams
-        })
-      },
-      reset () {
-        // 取消选中
-        this.selectedRowKeys = []
-        // 重置分页
-        this.pagination.current = this.pagination.defaultCurrent
-        if (this.paginationInfo) {
-          this.paginationInfo.current = this.pagination.defaultCurrent
-          this.paginationInfo.pageSize = this.pagination.defaultPageSize
-        }
-        // 重置列排序规则
-        this.sortedInfo = null
-        // 重置查询参数
-        this.queryParams = {}
-        // 清空时间选择
-        if (this.advanced) {
-          this.$refs.billDate.reset()
-        }
-        this.fetch()
-      },
-      handleTableChange (pagination, filters, sorter) {
-        this.paginationInfo = pagination
-        this.filteredInfo = filters
-        this.sortedInfo = sorter
-        this.fetch({
-          sortField: sorter.field,
-          sortOrder: sorter.order,
-          ...this.queryParams,
-          ...filters
-        })
-      },
-      fetch (params = {}) {
-        this.loading = true
-        if (this.paginationInfo) {
-          // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
-          this.pagination.current = this.paginationInfo.current
-          this.pagination.pageSize = this.paginationInfo.pageSize
-          params.pageSize = this.paginationInfo.pageSize
-          params.pageNum = this.paginationInfo.current
-        } else {
-          // 如果分页信息为空，则设置为默认值
-          params.pageSize = this.pagination.defaultPageSize
-          params.pageNum = this.pagination.defaultCurrent
-        }
-        this.$get('/crm/bill-record', {
-          ...params
-        }).then((r) => {
-          let data = r.data
-          const pagination = {...this.pagination}
-          pagination.total = data.total
-          this.dataSource = data.rows
-          this.pagination = pagination
-          // 数据加载完毕，关闭loading
-          this.loading = false
-        })
+    search () {
+      this.pagination.current = this.pagination.defaultCurrent
+      if (this.paginationInfo) {
+        this.paginationInfo.current = this.pagination.defaultCurrent
+        this.paginationInfo.pageSize = this.pagination.defaultPageSize
       }
+      let {sortedInfo} = this
+      let sortField, sortOrder
+      // 获取当前列的排序和列的过滤规则
+      if (sortedInfo) {
+        sortField = sortedInfo.field
+        sortOrder = sortedInfo.order
+      }
+      this.fetch({
+        sortField: sortField,
+        sortOrder: sortOrder,
+        ...this.queryParams
+      })
+    },
+    reset () {
+      // 取消选中
+      this.selectedRowKeys = []
+      // 重置分页
+      this.pagination.current = this.pagination.defaultCurrent
+      if (this.paginationInfo) {
+        this.paginationInfo.current = this.pagination.defaultCurrent
+        this.paginationInfo.pageSize = this.pagination.defaultPageSize
+      }
+      // 重置列排序规则
+      this.sortedInfo = null
+      // 重置查询参数
+      this.queryParams = {}
+      // 清空时间选择
+      if (this.advanced) {
+        this.$refs.billDate.reset()
+      }
+      this.fetch()
+    },
+    handleTableChange (pagination, filters, sorter) {
+      this.paginationInfo = pagination
+      this.filteredInfo = filters
+      this.sortedInfo = sorter
+      this.fetch({
+        sortField: sorter.field,
+        sortOrder: sorter.order,
+        ...this.queryParams,
+        ...filters
+      })
+    },
+    fetch (params = {}) {
+      this.loading = true
+      if (this.paginationInfo) {
+        // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
+        this.pagination.current = this.paginationInfo.current
+        this.pagination.pageSize = this.paginationInfo.pageSize
+        params.pageSize = this.paginationInfo.pageSize
+        params.pageNum = this.paginationInfo.current
+      } else {
+        // 如果分页信息为空，则设置为默认值
+        params.pageSize = this.pagination.defaultPageSize
+        params.pageNum = this.pagination.defaultCurrent
+      }
+      this.$get('/crm/bill-record', {
+        ...params
+      }).then((r) => {
+        let data = r.data
+        const pagination = {...this.pagination}
+        pagination.total = data.total
+        this.dataSource = data.rows
+        this.pagination = pagination
+        // 数据加载完毕，关闭loading
+        this.loading = false
+      })
     }
   }
+}
 </script>
 
 <style lang="less" scoped>
